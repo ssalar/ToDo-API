27b9c4fc09eb9184035dacc7ae5e3443d22a7c95
Salar <ssalar8998@gmail.com>
2022-01-18 15:22:03 -0600

    update

--- src/main/java/com/todo/api/controllers/ToDoController.java
+++ src/main/java/com/todo/api/controllers/ToDoController.java
@@ -7,8 +7,14 @@ package com.todo.api.controllers;
 import com.todo.api.dao.ToDoDao;
 import com.todo.api.models.ToDo;
 import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -29,5 +35,21 @@ public class ToDoController {
     public List<ToDo> all() {
         return dao.getAll();
     }
+    
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ToDo create(@RequestBody ToDo todo) {
+        return dao.add(todo);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<ToDo> findById(@PathVariable int id) {
+        ToDo result = dao.findById(id);
+        if (result == null) {
+            return new ResponseEntity(null, HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(result);
+    }
+
     
 }
